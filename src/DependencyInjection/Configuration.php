94c482c15d3cd4e12ebca424ac0e8b3d59662833
Torsten Burschka <torsten.burschka@invia.de>
2017-10-20 09:58:34 +0200

    ULN-103 - added basic symfony-bundle structure - added adapter and service entry - added tests

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Invia\CMI\JsonAdapterBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * {@inheritdoc}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        return new TreeBuilder();
+    }
+}
